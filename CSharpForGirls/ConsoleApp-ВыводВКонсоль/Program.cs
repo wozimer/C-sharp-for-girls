c2e56a7dbe37b5aa979850605133ba822b033633
Maxim Remizov <wozimer@gmail.com>
2018-02-14 16:48:18 +0300

    Шаблон для урока "Консоль как средство взаимодействия с миром."

---
+++ CSharpForGirls/ConsoleApp-ВыводВКонсоль/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp_ВыводВКонсоль
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Не менять ничего выше этой строки */
+            /*************************************/
+
+            Console.Write("Привет, мир!");
+            Console.WriteLine();
+
+            /*************************************/
+            /* Не менять ничего ниже этой строки */
+            Console.ReadLine();
+        }
+    }
+}

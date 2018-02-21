7a8832869068cf1ae3ab4d04dce18de2a665f00f
Maxim Remizov <wozimer@gmail.com>
2018-02-21 19:52:38 +0300

    Заготовка под Знакомство с переменными.

---
+++ CSharpForGirls/ConsoleApp1-ЗнакомствоСПеременными/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1_ЗнакомствоСПеременными
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine('Урок "Знакомство с переменными". Практика.');
+            /* Не менять ничего выше этой строки */
+            /*************************************/
+
+            int count;
+            count = 25;
+            Console.Write(count);
+
+            /*************************************/
+            /* Не менять ничего ниже этой строки */
+            Console.ReadLine();
+        }
+    }
+}

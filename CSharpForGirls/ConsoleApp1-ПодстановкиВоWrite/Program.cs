e1fe0a6847884e28880d5200b8264f61a01f077a
Maxim Remizov <wozimer@gmail.com>
2018-02-26 16:16:33 +0300

    Использование {0} во Write

---
+++ CSharpForGirls/ConsoleApp1-ПодстановкиВоWrite/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp1_ПодстановкиВоWrite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Урок 'Использование {0} во Write'. Практика.");
+            Console.WriteLine();
+
+            /* Не менять ничего выше этой строки */
+            /*************************************/
+
+            string areaname;
+            int amount;
+            int total;
+            double percent;
+
+            areaname = "Д13";
+            amount = 7;
+            total = 1035;
+            percent = 2;
+
+            Console.Write("На участке ");
+            Console.Write(areaname);
+            Console.Write(" засохло ");
+            Console.Write(amount);
+            Console.Write(" деревьев из ");
+            Console.Write(total);
+            Console.Write(", что составляет ");
+            Console.Write(percent);
+            Console.Write("% от общего числа сушняка.");
+            Console.WriteLine();
+
+            Console.Write("На участке {0} засохло {1} деревьев из {2}, что составляет {3}% от общего числа сушняка.", areaname, amount, total, percent);
+            Console.WriteLine();
+
+            /*************************************/
+            /* Не менять ничего ниже этой строки */
+            Console.ReadLine();
+        }
+    }
+}

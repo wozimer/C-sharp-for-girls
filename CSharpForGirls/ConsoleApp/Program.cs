a80dc8dbad7a7ede185ab78a63ce205872d1e5be
Maxim Remizov <wozimer@gmail.com>
2018-03-13 12:15:24 +0300

    Общее консольное приложение для выполнения заданий

---
+++ CSharpForGirls/ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Консольное приложение для выполнения заданий.");
+            Console.WriteLine();
+
+            /* Не менять ничего выше этой строки */
+            /*************************************/
+
+
+            
+            /*************************************/
+            /* Не менять ничего ниже этой строки */
+
+            Console.ReadLine();
+        }
+    }
+}
